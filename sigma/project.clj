5898fde09549d48486d068ffaf98d6a38d065160
Michael Kropat <mail@michael.kropat.name>
2019-12-07 11:11:11 -0500

    Create Clojure app
    lein new app sigma

---
+++ sigma/project.clj
@@ -0,0 +1,7 @@
+(defproject sigma "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :main ^:skip-aot sigma.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
